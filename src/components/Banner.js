6a28dfb104da9f2d934871ef080efa56c634a7d7
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-03-11 15:10:14 +0100

    ajout logo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,10 @@
 import './style/Banner.css'
+import logo from '../assets/logo.png'
 
 function Banner() {
   <div className="lmj-title">
+    <img src={logo} alt="logo" className='lmj-logo'/>
+    <h1>Mon panier</h1>
     <h1>La maison jungle</h1>
   </div> 
 }
