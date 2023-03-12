d9abcfb9652344d77d2dcdef40f6af8a50924aa5
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-03-12 02:34:43 +0100

    lots of change

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,20 @@ import '../styles/Layout.css'
 import { useState } from 'react'
 
 function App() {
-    const [cart, updateCart] = useState([])
-    
-    return (
-        <div>
-            <Banner>
-                <img src={logo} alt='La maison jungle' className='lmj-logo' />
-                <h1 className='lmj-title'>La maison jungle</h1>
-            </Banner>
-            <div className='lmj-layout-inner'>
-                <Cart cart={cart} updateCart={updateCart} />
-                <ShoppingList cart={cart} updateCart={updateCart} />
-            </div>
-            <Footer />
-        </div>
-    )
+	const [cart, updateCart] = useState([])
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
 export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,35 +1,60 @@
+import { useState } from 'react'
 import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
+import Categories from './Categories'
 import '../styles/ShoppingList.css'
-import updateCart from './App'
-import cart from './Cart'
 
 /*Ce code définit un composant de fonction ShoppingList en React. À l’intérieur de ce composant, une constante categories est définie en utilisant la méthode reduce sur un tableau plantList. Cette méthode crée un nouveau tableau avec toutes les catégories uniques de plantes présentes dans le tableau plantList.
 La fonction passée à la méthode reduce prend deux arguments: l’accumulateur (acc) et l’élément actuel du tableau (plant). Si l’accumulateur contient déjà la catégorie de la plante actuelle, il est renvoyé tel quel. Sinon, la catégorie est ajoutée à l’accumulateur en utilisant la méthode concat.*/ 
-function ShoppingList() {
+function ShoppingList({ cart, updateCart }) {
+	const [activeCategory, setActiveCategory] = useState('')
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
 
-    return (
-        <div className='lmj-shopping-list'>
-            <ul>
-                {categories.map((cat) => (
-                <li key={cat}>{cat}</li>
-                ))}
-            </ul>
-            <ul className='lmj-plant-list'>
-                {plantList.map(({ id, cover, name, water, light }) => (
-                    <div key={id}>
-                        <PlantItem cover={cover} name={name} water={water} light={light} />
-                        <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
-                    </div>
-                ))}
-            </ul>
-        </div>
-    )
+	function addToCart(name, price) {
+		const currentPlantSaved = cart.find((plant) => plant.name === name)
+		if (currentPlantSaved) {
+			const cartFilteredCurrentPlant = cart.filter(
+				(plant) => plant.name !== name
+			)
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantSaved.amount + 1 }
+			])
+		} else {
+			updateCart([...cart, { name, price, amount: 1 }])
+		}
+	}
+
+	return (
+		<div className='lmj-shopping-list'>
+			<Categories
+				categories={categories}
+				setActiveCategory={setActiveCategory}
+				activeCategory={activeCategory}
+			/>
+
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light, price, category }) =>
+					!activeCategory || activeCategory === category ? (
+						<div key={id}>
+							<PlantItem
+								cover={cover}
+								name={name}
+								water={water}
+								light={light}
+								price={price}
+							/>
+							<button onClick={() => addToCart(name, price)}>Ajouter</button>
+						</div>
+					) : null
+				)}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
